749618ce6996b14671f8a994f9d46f4c14326fdc
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-04-14 00:55:16 +0000

    ajustados

--- deploy/src/js/template-loader.js
+++ deploy/src/js/template-loader.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Carrega o cabeçalho
-    fetch("../templates/header.html")
+    fetch("src/templates/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-container").innerHTML = data;
         });
 
     // Carrega o rodapé
-    fetch("../templates/footer.html")
+    fetch("src/templates/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
